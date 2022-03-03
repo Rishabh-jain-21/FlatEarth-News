80e29e8af92034a5f287577a1da64a7f71b43790
Rishabh Jain <nonu47596@gmail.com>
2022-03-03 16:20:47 +0530

    Fixing bugs of spinner component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import loader from "./loader.gif";
 export class Spinner extends Component {
     render() {
         return (
-            <div className='text-center my-3'>
+            <div className='text-center my-3' style={{ height: "80vh", width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
                 <img src={loader} alt="loader" style={{ width: "50px" }} />
             </div>
         )
